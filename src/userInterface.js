8e8745850b6dcd6d883e65af618d194645c7a294
Nick Chapa <nicky.chapa@hotmail.com>
2026-08-16 06:01:45 -0500

    display project title in html

--- src/userInterface.js
+++ src/userInterface.js
@@ -2,11 +2,16 @@ const body = document.querySelector('body');
 
 export function displayProject(project){
     console.log('userinterface.js')
+    
+    const projectH3 = document.createElement('h3');
+    projectH3.textContent = project.title;
+    body.append(projectH3);
     console.log(`project: ${project.title}`);
+
     for(let item of project.todoArr){
-        const newP = document.createElement('p');
-        newP.textContent = item.title;
-        body.append(newP);
+        const todoPara = document.createElement('p');
+        todoPara.textContent = item.title;
+        body.append(todoPara);
         console.log(`item: ${item.title}`);
     }
 }
